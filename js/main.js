471267392e04828c9b05a289d0d41bfbd4ad5bb4
owozniak <aleksander.wozniak0@gmail.com>
2023-04-20 08:34:04 +0200

    zad 27-31

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
-console.log('akademia 108');
+alert('Skrypt działa prawidłowo!');
+
+let num = 12;
+if (num > 10) {
+    console.log('Podana liczba jest wieksza niz 10');
+}
+else {
+    console.log('Podana liczba nie jest wieksza niz 10');
+}
+
+let firstNumber = 1;
+let secondNumber = 2;
+function add(num1,num2) {
+    let sum = num1 + num2;
+    return sum;
+}
+let par = document.querySelector('p');
+par.textContent = 'dowolny tekst';
+console.log(par);
+let button = document.querySelector('button');
+button.onclick = function() {alert('Klik dziala!')};
